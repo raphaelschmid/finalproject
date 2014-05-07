67938c6e9dffec3f2ce060a6fc802ea2ee923b35
6ujjzuzju <srrwe@sffsef.com>
2014-05-07 16:27:24 +0200

    schnittstelle

---
+++ KinectYp/MotionFunctions.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinectYp
+{
+    class MotionFunctions
+    {
+         static int tn = 50;
+        static int tl = 70;
+
+        public static void SendAction(string action)
+        {
+            var client = new UdpClient();
+            IPAddress address = IPAddress.Parse(IPAddress.Broadcast.ToString());
+            client.Connect(address, 9876);
+            byte[] sendBytes = Encoding.ASCII.GetBytes(action);
+            client.Send(sendBytes, sendBytes.GetLength(0));
+        }
+
+        public static String Left()
+        {
+            return "dg;" + tn + ";ug;" + tl + ";";
+        }
+
+        public static String Right()
+        {
+            return "dh;" + tn + ";uh;" + tl + ";";
+        }
+
+        public static String Up()
+        {
+            return "dz;" + tn + ";uz;" + tl + ";";
+        }
+
+        public static String Down()
+        {
+            return "db;" + tn + ";ub;" + tl + ";";
+        }
+
+        public static String MPunch()
+        {
+            return "ds;" + tn + ";us;" + tl + ";";
+        }
+
+        public static String LPunch()
+        {
+            return "da;" + tn + ";ua;" + tl + ";";
+        }
+
+        public static String HPunch()
+        {
+            return "dd;" + tn + ";ud;" + tl + ";";
+        }
+
+        public static String TripplePunch()
+        {
+            return "dq;" + tn + ";uq;" + tl + ";";
+        }
+
+        public static String MKick()
+        {
+            return "dx;" + tn + ";ux;" + tl + ";";
+        }
+
+        public static String LKick()
+        {
+            return "dy;" + tn + ";uy;" + tl + ";";
+        }
+
+        public static String HKick()
+        {
+            return "dc;" + tn + ";uc;" + tl + ";";
+        }
+
+        public static String TrippleKick()
+        {
+            return "de;" + tn + ";ue;" + tl + ";";
+        }
+
+        public static String Qfc()
+        {
+            return "db;" + tn + ";dh;" + tn + ";ub;" + tn + ";uh;" + tl + ";";
+        }
+
+        public static String Qbc()
+        {
+            return "db;" + tn + ";dg;" + tn + ";ub;" + tn + ";ug;" + tl + ";";
+        }
+
+        public static String HalfCircle()
+        {
+            return "dg;" + tn + ";db;" + tn + ";ug;" + tn + ";dh;" + tn + ";ub;" + tn + ";uh;" + tl + ";";
+        }
+
+        public static String FullCircle()
+        {
+            return "dh;" + tn + ";db;" + tn + ";uh;" + tn + ";dg;" + tn + ";ub;" + tn + ";dz;" + tn + ";ug;" + tn + ";dh;" + tn + ";uz;" + tn + ";uh;" + tl + ";";
+        }
+
+        public static String ForwardZ()
+        {
+            return "dh;" + tn + ";uh;" + tn + ";db;" + tn + ";dh;" + tn + ";ub;" + tn + ";uh;" + tl + ";";
+        }
+
+        public static String BackwardZ()
+        {
+            return "dg;" + tn + ";ug;" + tn + ";db;" + tn + ";dg;" + tn + ";ub;" + tn + ";ug;" + tl + ";";
+        }
+    }
+}
---
+++ KinectYp/UDPServer.java
@@ -0,0 +1,65 @@
+﻿import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.lang.reflect.Field;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+class UDPServer
+{
+    static Robot robot;
+
+    public static void main(String args[]) throws Exception {
+        robot = new Robot();
+        DatagramSocket serverSocket = new DatagramSocket(9876);
+        byte[] receiveData = new byte[1024];
+        byte[] sendData;
+        while (true) {
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            serverSocket.receive(receivePacket);
+            String sentence = new String(receivePacket.getData());
+            System.out.println(">>>RECEIVED: " + sentence);
+            parseMessage(sentence);
+            InetAddress IPAddress = receivePacket.getAddress();
+            int port = receivePacket.getPort();
+            String capitalizedSentence = sentence.toUpperCase();
+            sendData = capitalizedSentence.getBytes();
+            DatagramPacket sendPacket =
+                    new DatagramPacket(sendData, sendData.length, IPAddress, port);
+            serverSocket.send(sendPacket);
+        }
+    }
+
+    public static void parseMessage(String message) throws InterruptedException, NoSuchFieldException, IllegalAccessException {
+        for (String messagePart : message.split(";")) {
+            messagePart = messagePart.trim();
+            if (messagePart.length() >= 2) {
+                if (messagePart.matches("^\\d+$")) {
+                    System.out.format("sleeping: %s ms.\n", messagePart);
+                    Thread.sleep(Integer.valueOf(messagePart));
+                } else {
+                    if (messagePart.substring(0, 1).equals("d")) {
+                        System.out.format("pressing:  %s ms.\n", messagePart.charAt(1));
+                        robot.keyPress(getKeyEvent(messagePart.charAt(1)));
+                    } else if (messagePart.substring(0, 1).equals("u")) {
+                        System.out.format("releasing: %s ms.\n", messagePart.charAt(1));
+                        robot.keyRelease(getKeyEvent(messagePart.charAt(1)));
+                    }
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    public static int getKeyEvent(Character c) throws NoSuchFieldException, IllegalAccessException {
+        String prefix;
+        if (Character.isDigit(c)) {
+            prefix = "VK_NUMPAD" + c;
+        } else {
+            prefix = "VK_" + Character.toUpperCase(c);
+        }
+        Field f = KeyEvent.class.getField(prefix);
+        f.setAccessible(true);
+        return (Integer) f.get(null);
+    }
+}
